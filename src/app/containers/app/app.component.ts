c4ddc2f6b24220b4324125d24b99841c03d21944
Mateusz Czarnik <mamarok@gmail.com>
2018-05-15 21:31:50 +0200

    Reactive Store and AngularFire Observables

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from "store";
+import {AuthService, User} from "../../../auth/shared/service/auth/auth.service";
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div>
-      Hello Ultimate Angular!
-        <div class="wrapper">
-            <router-outlet></router-outlet>
+    selector: 'app-root',
+    styleUrls: ['app.component.scss'],
+    template: `
+        <div>
+            {{ user$ | async | json }}
+            Hello Ultimate Angular!
+            <div class="wrapper">
+                <router-outlet></router-outlet>
+            </div>
         </div>
-    </div>
-  `
+    `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+    user$: Observable<User>;
+    subscription: Subscription;
+
+    constructor(
+        private store: Store,
+        private authService: AuthService,
+    ) {
+    }
+
+    ngOnInit() {
+        this.subscription = this.authService.auth$.subscribe();
+        this.user$ = this.store.select<User>('user')
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
