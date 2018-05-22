f861f0357cce8fe59d7f5b4e2c95e4358d60df08
Mateusz Czarnik <mamarok@gmail.com>
2018-05-22 22:59:08 +0200

    Stateless components and removing items

--- src/health/shared/components/list-item/list-item.component.ts
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -1,20 +1,69 @@
-import {Component, Input} from "@angular/core";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
     selector: 'list-item',
     styleUrls: ['list-item.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
         <div class="list-item">
-            {{ item | json }}
+            <a [routerLink]="getRoute(item)">
+                <p class="list-item__name">{{item.name}}</p>
+                <p class="list-item__ingredients">
+                    <span>
+                        {{ item.ingredients }}
+                    </span>
+                </p>
+            </a>
+            <div
+                    class="list-item__delete"
+                    *ngIf="toggled"
+            >
+                <p>Delete item?</p>
+                <button
+                        type="button"
+                        class="confirm"
+                        (click)="removeItem()"
+                >Yes
+                </button>
+                <button
+                        type="cancel"
+                        class="confirm"
+                        (click)="toggle()"
+                >No
+                </button>
+            </div>
+            <button
+                    type="button"
+                    class="trash"
+                    (click)="toggle()"
+            ><img src="/img/remove.svg" alt=""></button>
         </div>
     `
 })
 
 export class ListItemComponent {
+
+    toggled = false;
+
     @Input()
     item: any;
 
+    @Output()
+    remove = new EventEmitter<any>();
+
     constructor() {
 
+    }
+
+    getRoute(item: any) {
+        return ['../meals', item.$key]
+    }
+
+    removeItem(){
+        this.remove.emit(this.item)
+    }
+
+    toggle() {
+        this.toggled = !this.toggled
     }
 }
