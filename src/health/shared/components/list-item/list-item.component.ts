0009840ab2bd5a1dc3fc7767987a010bb1470b17
Mateusz Czarnik <mamarok@gmail.com>
2018-06-03 14:17:17 +0200

    Custom Workout / Meal Pipes

--- src/health/shared/components/list-item/list-item.component.ts
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -9,8 +9,11 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@
           <a [routerLink]="getRoute(item)">
               <p class="list-item__name">{{item.name}}</p>
               <p class="list-item__ingredients">
-                  <span>{{ item.ingredients }}</span>
+                  <span *ngIf="item.ingredients; else showWorkout">{{ item.ingredients | join}}</span>
               </p>
+              <ng-template #showWorkout>
+                  <span>{{ item | workout }}</span>
+              </ng-template>
           </a>
           <div
             class="list-item__delete"
@@ -54,7 +57,7 @@ export class ListItemComponent {
   }
 
   getRoute(item: any) {
-    return ['../meals', item.$key]
+    return [`../${item.ingredients ? 'meals' : 'workouts'}`, item.$key]
   }
 
   removeItem() {
--- src/health/shared/shared.module.ts
+++ src/health/shared/shared.module.ts
@@ -12,28 +12,36 @@ import {ListItemComponent} from './components/list-item/list-item.component'
 import {MealsService} from './services/meals/meals.service';
 import {WorkoutsService} from "./services/workouts/workouts.service";
 
+// pipes
+import {JoinPipe} from "./pipes/join.pipe";
+import {WorkoutPipe} from "./pipes/workout.pipe";
+
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule,
-        AngularFireDatabaseModule
-    ],
-    declarations: [
-        ListItemComponent
-    ],
-    exports: [
-        ListItemComponent
-    ]
+  imports: [
+    CommonModule,
+    RouterModule,
+    AngularFireDatabaseModule
+  ],
+  declarations: [
+    ListItemComponent,
+    JoinPipe,
+    WorkoutPipe
+  ],
+  exports: [
+    ListItemComponent,
+    JoinPipe,
+    WorkoutPipe
+  ]
 })
 
 export class SharedModule {
-    static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: SharedModule,
-            providers: [
-                MealsService,
-                WorkoutsService
-            ]
-        }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        MealsService,
+        WorkoutsService
+      ]
     }
+  }
 }
